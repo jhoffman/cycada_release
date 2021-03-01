940d4b3ccf448cd9112a9233638019b1e184f2e9
Shreyas Niradi <shreyas.niradi@gmail.com>
2021-02-28 23:36:09 -0800

    add gta->cityscapes cycle gan training

---
+++ train_gta2cityscapes.sh
@@ -0,0 +1,8 @@
+CUDA_VISIBLE_DEVICES=0 python ./cyclegan/train.py --name gta2cityscapes \
+    --resize_or_crop="scale_width_and_crop" \
+    --loadSize=1024 --fineSize=400 \
+    --model cycle_gan \
+    --lambda_identity 1.0 \
+    --batchSize 16 \
+    --dataset_mode unaligned --dataroot /mnt/data/cyclegan_data/ \
+    --which_direction AtoB
