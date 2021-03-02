b7a7fd1fce63b070cbe9464d477a863e2d072c11
Shreyas Niradi <shreyas.niradi@gmail.com>
2021-03-01 19:41:16 -0800

    reduce batch size for 1080Ti

--- train_gta2cityscapes.sh
+++ train_gta2cityscapes.sh
@@ -3,6 +3,6 @@ CUDA_VISIBLE_DEVICES=0 python ./cyclegan/train.py --name gta2cityscapes \
     --loadSize=1024 --fineSize=400 \
     --model cycle_gan \
     --lambda_identity 1.0 \
-    --batchSize 8 \
+    --batchSize 4 \
     --dataset_mode unaligned --dataroot /mnt/data/cyclegan_data/ \
     --which_direction AtoB
