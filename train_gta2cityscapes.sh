b0edf88c3f2128d4f1814f487735ae2e61fa684c
Shreyas Niradi <shreyas.niradi@gmail.com>
2021-03-02 16:47:02 -0800

    add train/test scripts

--- train_gta2cityscapes.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python ./cyclegan/train.py --name gta2cityscapes \
-    --resize_or_crop="scale_width_and_crop" \
-    --loadSize=1024 --fineSize=400 \
-    --model cycle_gan \
-    --lambda_identity 1.0 \
-    --batchSize 4 \
-    --dataset_mode unaligned --dataroot /mnt/data/cyclegan_data/ \
-    --which_direction AtoB
